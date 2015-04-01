e96b2d56cb8b3af51e2d0ae2a5e0d95f137b3990
Jacob Kimmel <jacobkimmel@gmail.com>
2015-04-01 17:04:39 -0400

    Updated easy_eleven.py and documentation; added easy_eleven.py test data

---
+++ easy_eleven.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import sys
+import pandas as pd
+import scipy
+import eleven
+
+'''
+USAGE:
+
+python easy_eleven.py qPCR_data_file.csv Reference_Genes.csv Control_sample_name output_path
+
+
+DATA FORMATS:
+
+qPCR_data_file.csv
+---
+Sample,Target,Cq
+Sample1,Target1,Cq
+...
+SampleN,TargetN,Cq
+NTC,TargetN,Cq
+
+
+Reference_Genes.csv
+---
+Gene1
+Gene2
+Gene3
+Gene4
+
+RETURNS:
+Output of rank_targets()
+'''
+
+writer = pd.ExcelWriter(sys.argv[4])
+df = pd.read_csv(sys.argv[1])
+print df
+
+censored = eleven.censor_background(df)
+print censored
+censored.to_excel(writer,'Censored_Data')
+
+
+#Read Reference_Genes.csv 
+with open('ref_genes.csv', 'r') as f:
+	ref_genes = [line.strip() for line in f]
+
+constant_sample = str(sys.argv[3])
+
+ranked = eleven.rank_targets(censored, ref_genes, constant_sample)
+
+print ranked
+
+nf = eleven.calculate_nf(censored, ranked.loc[0:2,'Target'],constant_sample)
+
+print nf
+
+censored['RelExp'] = eleven.expression_nf(censored, nf, constant_sample)
+
+censored.to_excel('Relative_Exp')
+ranked.to_excel(writer,'Ranked Genes')
+df.to_excel(writer, 'Raw_Data')
+writer.save()
+
+
